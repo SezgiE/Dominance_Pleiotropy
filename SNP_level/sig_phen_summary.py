55e33e07ec1223b891296b4b28656b1c14106c85
Sezgi <sezgiiercan@gmail.com>
2026-05-15 02:31:12 +0200

    phenotype summary v2

--- SNP_level/sig_phen_summary.py
+++ SNP_level/sig_phen_summary.py
@@ -2,7 +2,7 @@ import pandas as pd
 import glob
 import os
 
-def sig_phen_summary(trait_df, phen_info_df, trait_code):
+def sig_phen_summary(trait_df, phen_info_df, add_pleiotropic_snps, dom_pleiotropic_snps, trait_code):
     """
     Summarizes significant SNPs per chromosome for a given trait.
     """
@@ -17,12 +17,20 @@ def sig_phen_summary(trait_df, phen_info_df, trait_code):
     trait_df['is_add'] = trait_df['add_sig'] == 1
     trait_df['is_dom'] = trait_df['dom_sig'] == 2
     trait_df['is_both'] = trait_df[trait_code] == 3
-    
+
+    # Is pleiotropic for add/dom or both
+    trait_df['is_add_pleiotropic'] = trait_df['is_add'] & trait_df['variant'].isin(add_pleiotropic_snps)
+    trait_df['is_dom_pleiotropic'] = trait_df['is_dom'] & trait_df['variant'].isin(dom_pleiotropic_snps)
+    trait_df['is_both_pleiotropic'] = trait_df['is_add_pleiotropic'] & trait_df['is_dom_pleiotropic']
+
     # Group by chr and sum the boolean columns
     summary = trait_df.groupby(['chr', 'is_mhc']).agg(
         add_sig=('is_add', 'sum'),
         dom_sig=('is_dom', 'sum'),
-        both_sig=('is_both', 'sum')
+        both_sig=('is_both', 'sum'),
+        add_pleio =('is_add_pleiotropic', 'sum'),
+        dom_pleio =('is_dom_pleiotropic', 'sum'),
+        both_pleio =('is_both_pleiotropic', 'sum')
     ).reset_index()
     
     # Add phenotype_code for the merge
@@ -35,7 +43,7 @@ def sig_phen_summary(trait_df, phen_info_df, trait_code):
     summary = pd.merge(phen_info, summary, on='phenotype_code', how='inner')
     
     # Reorder columns: phen_info columns first, then chr, then the signal counts
-    final_cols = list(phen_info.columns) + ['chr', 'is_mhc', 'add_sig', 'dom_sig', 'both_sig']
+    final_cols = list(phen_info.columns) + ['chr', 'is_mhc', 'add_sig', 'dom_sig', 'both_sig', 'add_pleio', 'dom_pleio', 'both_pleio']
     
     return summary[final_cols]
 
@@ -45,6 +53,7 @@ if __name__ == "__main__":
     # Directories
     trait_df_dir = "/Users/sezgi/Documents/dominance_pleiotropy/loci_level/sumstats_QCed"
     phen_info_path = "/Users/sezgi/Documents/dominance_pleiotropy/UKB_sumstats_Neale/supp_table1.tsv"
+    all_sig_snps_path = "/Users/sezgi/Documents/dominance_pleiotropy/SNP_level/significant_SNPs/all_sig_SNPs.tsv.gz"
     output_path = "/Users/sezgi/Documents/dominance_pleiotropy/SNP_level/results/phen_summary.tsv"
 
     # Load phenotype metadata
@@ -52,6 +61,13 @@ if __name__ == "__main__":
                    "source", "n_non_missing", "n_missing", "n_controls", "n_cases"]
     
     phen_info_df = pd.read_csv(phen_info_path, sep='\t', usecols=cols_to_use)
+
+    # Load all significant SNPs (if needed for further filtering, not currently used in the summary)
+    all_sig_snps_df = pd.read_csv(all_sig_snps_path, sep='\t', compression='gzip', 
+                                  usecols=["variant", "add_sig_total", "dom_sig_total"])
+    
+    add_pleiotropic_snps = set(all_sig_snps_df[all_sig_snps_df['add_sig_total'] > 1]['variant'])
+    dom_pleiotropic_snps = set(all_sig_snps_df[all_sig_snps_df['dom_sig_total'] > 1]['variant'])
     
     # Find all "_sig_SNPs.tsv.bgz" files
     file_pattern = os.path.join(trait_df_dir, "*_sig_SNPs.tsv.bgz")
@@ -70,7 +86,7 @@ if __name__ == "__main__":
         trait_df = pd.read_csv(file_path, sep='\t', compression='gzip')
         
         # Get summary
-        phen_summary = sig_phen_summary(trait_df, phen_info_df, trait_code)
+        phen_summary = sig_phen_summary(trait_df, phen_info_df, add_pleiotropic_snps, dom_pleiotropic_snps, trait_code)
         phen_summaries.append(phen_summary)
         
     # Combine all summaries and save
